3dfbccf85893d7adc73816c9c6b84c2a6df8d573
Awe Ayomidipupo <pupoawe@gmail.com>
2024-01-25 15:17:31 +0100

    setup vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <GoogleAnalytics gaId="G-171SE90677" />
+      <Analytics />
     </>
   );
 }
